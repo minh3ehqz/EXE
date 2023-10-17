282d06a689182b3ab41360c37b5ef95fbc4b2872
minh3ehqz <minh3ehqz@gmail.com>
2023-10-17 19:59:54 +0700

    x

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Header from "./components/heaeder/Header";
 import Home from "./components/home/Home";
 import Menu from "./components/Menu/Menu";
+import WelcomePopup from "./components/Emotion/EmotionPopup";
 
 function App() {
   return (
     <>
       <Header />
+      <WelcomePopup/>
       <Home />
       <Menu/>
     </>
